9023e046de119797271ad80659a2ece2d7ee9108
Loren Bandiera <lorenb@mmgsecurity.com>
2005-06-21 01:34:56 +0000

    Updating autogen.sh and configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,58 @@
 #!/bin/sh
+
+PROJECT=lat
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+AUTOMAKE=automake-1.8
+ACLOCAL=aclocal-1.8
+
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+        AUTOMAKE=automake
+        ACLOCAL=aclocal
+}
+
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+(grep "^AM_PROG_LIBTOOL" configure.ac >/dev/null) && {
+  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have \`libtool' installed to compile $PROJECT."
+    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
 intltoolize --force --copy || exit 1
-autoreconf -v --force --install || exit 1
+
+echo "Running $ACLOCAL ..."
+$ACLOCAL
+if grep "^AM_CONFIG_HEADER" configure.ac >/dev/null; then
+	echo "Running autoheader..."
+	autoheader
+fi
+echo "Running $AUTOMAKE --gnu ..."
+$AUTOMAKE --add-missing --gnu 
+echo "Running autoconf ..."
+autoconf
+
 ./configure --enable-maintainer-mode "$@"
 
