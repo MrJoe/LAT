6d96a9a003599251f59b9017efdbe1de0ad96a76
Loren Bandiera <lorenb@mmgsecurity.com>
2005-07-02 04:54:39 +0000

    Start of gnome-keyring support

--- lat/ProfileManager.cs
+++ lat/ProfileManager.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections;
 using System.IO;
 using System.Xml;
+using GnomeKeyring;
 
 namespace lat
 {
@@ -107,9 +108,24 @@ namespace lat
 							int.Parse (r.GetAttribute ("port")),
 							r.GetAttribute ("base"),
 							r.GetAttribute ("user"),
-							r.GetAttribute ("pass"),
+							"",
+//							r.GetAttribute ("pass"),
 							bool.Parse (r.GetAttribute ("ssl")),
 							r.GetAttribute ("server_type"));
+
+						GnomeKeyring.Result gkr;
+						NetworkPasswordData[] list;
+						gkr = GnomeKeyring.Global.FindNetworkPassword(
+						 cp.User,
+						 null, null, null, null, null, 0, out list
+						);
+
+						Logger.Log.Debug ("gnome-keyring-result: {0}", gkr);
+						
+						foreach (NetworkPasswordData i in list) 
+						{
+							 cp.Pass = i.Password;
+						}
 				
 						_profiles.Add (cp.Name, cp);	
 					} 
@@ -123,6 +139,11 @@ namespace lat
 			}
 		}
 		
+		static void myCallback (Result result, uint val) 
+		{
+			Logger.Log.Debug ("gnome-keyring-callback: result: {0} - ID: {1}", result, val);
+		}
+
 		public void saveProfiles ()	
 		{	
 			XmlTextWriter writer = new XmlTextWriter(_configFile,
@@ -144,11 +165,26 @@ namespace lat
 				writer.WriteAttributeString ("port", cp.Port.ToString());
 				writer.WriteAttributeString ("base", cp.LdapRoot);
 				writer.WriteAttributeString ("user", cp.User);
-				writer.WriteAttributeString ("pass", cp.Pass);
+//				writer.WriteAttributeString ("pass", cp.Pass);
 				writer.WriteAttributeString ("ssl", cp.SSL.ToString());
 				writer.WriteAttributeString ("server_type", cp.ServerType);
 				
 	        		writer.WriteEndElement();
+
+				OperationGetIntCallback theCallback = new OperationGetIntCallback (myCallback);
+
+				GnomeKeyring.Global.SetNetworkPassword(
+					 null, 				// keyring
+					cp.User,			// user
+					 null, 				// domain
+					cp.Host,			// server 
+					null, 				// objekt
+					"ldap", 			// protocol
+					null, 				// authtype
+					(uint)cp.Port, 			// port
+					cp.Pass,	 		// password
+					theCallback 			// callback
+				);
 			}
 	    		
 			writer.WriteEndElement();
