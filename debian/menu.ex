c27f87c1c4fd4231d7eb5900393faddc824377bf
Loren Bandiera <lorenb@mmgsecurity.com>
2006-03-22 18:14:37 +0000

    Updating debian/ makefile

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lat):needs=X11|text|vc|wm section=Apps/Tools\
-  title="LDAP Administration Tool" command="/usr/bin/lat"
--- debian/watch.ex
+++
@@ -1,4 +0,0 @@
-# watch control file for uscan
-# Site            Directory  Pattern               Version  Script
-people.mmgsecurity.com  /~lorenb/lat    lat-(.*)\.tar\.gz  debian   uupdate
-
